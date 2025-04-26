80887942717c892385e5e7d0a21ff84fd7556557
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2025-04-26 18:17:26 +0700

    Chore : comment data from app

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-import data from "../src/app/dashboard/data.json";
+// import data from "../src/app/dashboard/data.json";
 
 export default function Page() {
   return (
